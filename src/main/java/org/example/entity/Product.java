8d4d2a082ea83f1fc4eac4e8a4e84faff7571ef0
sargisEg <sargis.eg@gmail.com>
2023-04-02 18:42:01 +0400

    Created Product entity, repository and application.yml file

---
+++ src/main/java/org/example/entity/Product.java
@@ -0,0 +1,33 @@
+package org.example.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "PRODUCTS")
+public class Product {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "PRODUCTS_ID_SEQUENCE"
+    )
+    @SequenceGenerator(name = "PRODUCTS_ID_SEQUENCE")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Lob
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "short_description", nullable = false, length = 100)
+    private String shortDescription;
+
+}
